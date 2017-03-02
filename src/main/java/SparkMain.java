810069d59e02ea21da12e85c623bac8aee25b43b
Øyvind Bratvedt <oyvbr@stud.ntnu.no>
2017-03-02 15:37:36 +0100

    Added preliminary schemas. Needs to be modified in regarding what to use

--- src/main/java/SparkMain.java
+++ src/main/java/SparkMain.java
@@ -1,8 +1,39 @@
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import schemas.CalendarSchema;
+import schemas.ListingsSchema;
+import schemas.ReviewSchema;
 
 public class SparkMain {
+
+    private static Dataset<Row> getReviewDs(SparkSession sparkSession) {
+        return sparkSession
+                .read()
+                .schema(ReviewSchema.getReviewSchema())
+                .csv("airbnb_datasets/reviews_us.csv");
+    }
+
+    private static Dataset<Row> getListingDs(SparkSession sparkSession) {
+        return sparkSession
+                .read()
+                .schema(ListingsSchema.getListingsSchema())
+                .csv("airbnb_datasets/listings_us.csv");
+    }
+
+    private static Dataset<Row> getCalendarDs(SparkSession sparkSession) {
+        return sparkSession
+                .read()
+                .schema(CalendarSchema.getCalendarSchema())
+                .csv("airbnb_datasets/calendar_us.csv");
+    }
+
+    //Not working yet, need a parser.
+    private static Dataset<Row> getNeighbourhoodsDs(SparkSession sparkSession) {
+        return sparkSession
+                .read()
+                .json("airbnb_datasets/neighbourhoods.geojson");
+    }
 
     public static void main(String[] args) {
         SparkSession sparkSession = SparkSession
@@ -11,10 +42,7 @@ public class SparkMain {
                 .master("local[*]")
                 .getOrCreate();
 
-        Dataset<Row> ds = sparkSession
-                .read()
-                .option("header", true)
-                .csv("airbnb_datasets/reviews_us.csv");
+        Dataset<Row> ds = getReviewDs(sparkSession);
         ds.show();
 
     }
