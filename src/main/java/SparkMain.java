d16ef2aae0003fd6533e8fd303c22b8b978560db
Øyvind Bratvedt <oyvbr@stud.ntnu.no>
2017-03-02 14:06:25 +0100

    Added implementation of dataset

---
+++ src/main/java/SparkMain.java
@@ -0,0 +1,20 @@
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+public class SparkMain {
+
+    public static void main(String[] args) {
+        SparkSession sparkSession = SparkSession
+                .builder()
+                .appName("Airbnb analyis")
+                .master("local[*]")
+                .getOrCreate();
+
+        Dataset<Row> ds = sparkSession.read().csv("airbnb_datasets/reviews_us.csv");
+        ds.show();
+
+    }
+
+}
+
