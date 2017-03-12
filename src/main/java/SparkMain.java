2a64dd781f0b707c36e4380766eb6a3da4ae242e
Øyvind Bratvedt <oyvbr@stud.ntnu.no>
2017-03-12 16:25:16 +0100

    Task 1.3 3d-e

--- src/main/java/SparkMain.java
+++ src/main/java/SparkMain.java
@@ -2,6 +2,7 @@
 import org.apache.spark.sql.*;
 import org.apache.spark.sql.catalyst.encoders.RowEncoder;
 import org.scalatest.EncodedOrdering;
+import scala.Double;
 import scala.Tuple2;
 import scala.Tuple3;
 import schemas.ListingsSchema;
@@ -53,16 +54,16 @@ public class SparkMain {
     private static void distinctListings(Dataset<Row> listingsDs) {
         List<String> results = new ArrayList<>();
         Arrays.stream(listingsDs.schema().fieldNames())
-                .forEach( (fieldName) -> {
-                    long count = listingsDs.select( fieldName ).distinct().count();
+                .forEach((fieldName) -> {
+                    long count = listingsDs.select(fieldName).distinct().count();
                     results.add(fieldName + ":" + count);
                 });
-        results.forEach( result -> System.out.println(result));
+        results.forEach(result -> System.out.println(result));
     }
 
 
     /* Task 1.3 - 2 c) */
-    private static void listCities(Dataset<Row> listingsDs){
+    private static void listCities(Dataset<Row> listingsDs) {
         List<String> cities = new ArrayList<>();
         listingsDs
                 .select("city")
@@ -71,14 +72,14 @@ public class SparkMain {
     }
 
     /* Task 1.3 - 3 a) */
-    private static void avgCityPrice(Dataset<Row> listingsDs){
+    private static void avgCityPrice(Dataset<Row> listingsDs) {
         listingsDs.select("city", "price")
-                .map( row -> {
+                .map(row -> {
                     String priceString = row.getString(1);
                     float price = Float.valueOf(priceString
-                            .replace("$","")
-                            .replace(",",""));
-                    return new Tuple2<>(row.getString(0) ,price);
+                            .replace("$", "")
+                            .replace(",", ""));
+                    return new Tuple2<>(row.getString(0), price);
                 }, Encoders.tuple(Encoders.STRING(), Encoders.FLOAT()))
                 .toDF("city", "price")
                 .groupBy("city")
@@ -90,7 +91,7 @@ public class SparkMain {
     }
 
     /* Task 1.3 - 3 b) */
-    private static void avgRoomTypePrice(Dataset<Row> listingsDs){
+    private static void avgRoomTypePrice(Dataset<Row> listingsDs) {
         listingsDs.select("city", "room_type", "price")
                 .map(row -> new Tuple3<String, String, Float>(row.getAs("city"), row.getAs("room_type"), Float.valueOf(
                         ((String) row.getAs("price"))
@@ -98,7 +99,7 @@ public class SparkMain {
                                 .replace(",", "")))
                         , Encoders.tuple(Encoders.STRING(), Encoders.STRING(), Encoders.FLOAT()))
                 .toDF("city", "room_type", "price")
-                .groupBy( "city","room_type")
+                .groupBy("city", "room_type")
                 .agg(functions.avg("price"))
                 .orderBy(functions.col("city").asc())
                 .repartition(1)
@@ -106,23 +107,23 @@ public class SparkMain {
     }
 
     /* Task 1.3 - 3 c) */
-    private static void avgNumReviewPerMonth(Dataset<Row> reviewsDs, Dataset<Row> listingsDs){
+    private static void avgNumReviewPerMonth(Dataset<Row> reviewsDs, Dataset<Row> listingsDs) {
         Dataset<Row> joinedSet = reviewsDs
-                .select("listing_id","id", "date")
+                .select("listing_id", "id", "date")
                 .join(listingsDs.select("city", "id"),
-                reviewsDs.col("listing_id").equalTo(listingsDs.col("id").as("listing_id")))
-                .toDF("listing_id", "id","date","city", "listing_id_2");
+                        reviewsDs.col("listing_id").equalTo(listingsDs.col("id").as("listing_id")))
+                .toDF("listing_id", "id", "date", "city", "listing_id_2");
 
         joinedSet
                 .select("id", "date", "city")
-                .map( row -> {
+                .map(row -> {
                     String month = row.getDate(1)
                             .toString()
-                            .substring(0,7);
+                            .substring(0, 7);
                     return new Tuple3<>(row.getInt(0), month, row.getString(2));
                 }, Encoders.tuple(Encoders.INT(), Encoders.STRING(), Encoders.STRING()))
                 .toDF("id", "date", "city")
-                .groupBy("date","city")
+                .groupBy("date", "city")
                 .count()
                 .orderBy(functions.col("count").desc())
                 .coalesce(1)
@@ -130,11 +131,51 @@ public class SparkMain {
                 .csv("./output/avgNumReviewPerMonth");
     }
 
-    private void estimatedBookingsPerYear(Dataset<Row> reviewsDs){
+    /* Task 1.3 - 3 d) */
+    private static void estimatedBookingsPerYear(Dataset<Row> listingsDs) {
         double percentageWhoLeavesReview = 0.7;
+        int avgNoOfNights = 3;
 
+        listingsDs
+                .select("reviews_per_month", "city")
+                .groupBy("city")
+                .agg(functions.round(functions.sum(functions.col("reviews_per_month")
+                        .multiply(avgNoOfNights * 12)
+                        .divide(percentageWhoLeavesReview)
+                ))
+                        .alias("nightsPerYear"))
+                .orderBy(functions.col("nightsPerYear").desc())
+                .coalesce(1)
+                .write()
+                .csv("./output/estimatedBookingsPerYear");
 
+    }
 
+    /* Task 1.3 - 3 e)*/
+
+    private static void moneySpentPerYear(Dataset<Row> listingsDs) {
+        double percentageWhoLeavesReview = 0.7;
+        int avgNoOfNights = 3;
+
+        listingsDs
+                .select("city", "reviews_per_month", "price")
+                .map(row -> new Tuple3<>(
+                        row.getAs("city"), row.getAs("reviews_per_month"),
+                        Float.valueOf(
+                                ((String) row.getAs("price"))
+                                        .replace("$", "")
+                                        .replace(",", ""))
+                ), Encoders.tuple(Encoders.STRING(), Encoders.DOUBLE(), Encoders.FLOAT()))
+                .toDF("city", "reviews_per_month", "price")
+                .groupBy("city")
+                .agg(functions.sum(functions.col("reviews_per_month")
+                        .multiply(functions.col("price"))
+                        .multiply(avgNoOfNights * 12)
+                        .divide(percentageWhoLeavesReview)).as("moneySpent"))
+                .orderBy(functions.col("moneySpent").desc())
+                .coalesce(1)
+                .write()
+                .csv("./output/moneySpentPerYear");
     }
 
 
@@ -147,38 +188,17 @@ public class SparkMain {
 
         Dataset<Row> reviewsDs = getReviewDs(sparkSession);
         Dataset<Row> listingDs = getListingDs(sparkSession);
-        avgNumReviewPerMonth(reviewsDs, listingDs);
+        //moneySpentPerYear(listingDs);
+        //avgNumReviewPerMonth(reviewsDs, listingDs);
+        //estimatedBookingsPerYear(listingDs);
         //reviewsDs.show();
-
+        //listingDs.show();
         /*
         avgCityPrice(listingDs);
         avgRoomTypePrice(listingDs);*/
 
         //distinctListings(listingDs);
 
-        /*
-        ds.select("price")
-                .map( row -> Float.valueOf(
-                            ((String) row.getAs("price"))
-                            .replace("$", "")
-                            .replace(",", ""))
-
-                , Encoders.FLOAT() )
-                .filter( functions.col("value").gt(0) )
-                .agg( functions.avg("value") )
-                .show();*/
-
-        //System.out.println(val);
-
-        //ds.show();
-        /*
-
-        Dataset<Row> priceAvg = ds
-                .filter( functions.col("price").notEqual(0) )
-                .agg( functions.avg("price") );
-
-        priceAvg.show();
-*/
     }
 
 }
