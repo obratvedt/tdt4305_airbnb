1c7febf58b243dc6bb480660b30894cf99e7bfe4
Sigurd Grøneng <groneng93@gmail.com>
2017-03-16 17:30:05 +0100

    WIP: Made class for easy access to neighborhood
    Check new class Area. Also started on checking out
    how to check if point is inside polygon using the JST
    library but maybe we should do this outselves because
    somehow the TA in this course think that would a good
    way to grade BIG DATA projects.

---
+++ src/main/java/tasks/Area.java
@@ -0,0 +1,49 @@
+package tasks;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.util.List;
+
+/**
+ * Created by sigurd on 3/16/17.
+ */
+class Area {
+
+    JSONObject obj;
+
+    Area(JSONObject obj){
+        this.obj = obj;
+    }
+
+    public JSONObject getType(){
+        return (JSONObject) this.obj.get("type");
+    }
+
+    private JSONObject getProps(){
+        return (JSONObject) this.obj.get("properties");
+    }
+
+    public String getNeighbourhood(){
+        return (String) this.getProps().get("neighbourhood");
+    }
+
+    public String getNeighbourhoodGroup(){
+        return (String) this.getProps().get("neighbourhood_group");
+    }
+
+    public JSONObject getGeometry(){
+        return (JSONObject) this.obj.get("geometry");
+    }
+
+    public List getCoordinates(){
+        JSONArray firstArray = (JSONArray) this.getGeometry().get("coordinates");
+        JSONArray secondArray = (JSONArray) firstArray.get(0);
+        JSONArray finalArray = (JSONArray) secondArray.get(0);
+        return finalArray.subList(0,finalArray.size());
+    }
+
+    public String getGeoType(){
+        return (String) this.getGeometry().get("type");
+    }
+}
