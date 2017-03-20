ccd8505a4fbb4155f491e081ff854ab726b0afe6
Sigurd Grøneng <groneng93@gmail.com>
2017-03-20 15:37:04 +0100

    Task 6

--- src/main/java/tasks/Area.java
+++ src/main/java/tasks/Area.java
@@ -3,12 +3,13 @@ package tasks;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
+import java.io.Serializable;
 import java.util.List;
 
 /**
  * Created by sigurd on 3/16/17.
  */
-class Area {
+class Area implements Serializable {
 
     JSONObject obj;
 
@@ -36,7 +37,7 @@ class Area {
         return (JSONObject) this.obj.get("geometry");
     }
 
-    public List getCoordinates(){
+    public List<JSONArray> getCoordinates(){
         JSONArray firstArray = (JSONArray) this.getGeometry().get("coordinates");
         JSONArray secondArray = (JSONArray) firstArray.get(0);
         JSONArray finalArray = (JSONArray) secondArray.get(0);
