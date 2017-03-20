35e328d13bfa949777368455eaf0b44fcacaf14f
Sigurd Grøneng <groneng93@gmail.com>
2017-03-20 19:28:46 +0100

    AM WE FINITO NOW?

--- src/main/java/tasks/Task6.java
+++ src/main/java/tasks/Task6.java
@@ -1,19 +1,24 @@
 package tasks;
 
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import com.vividsolutions.jts.geom.*;
 import com.vividsolutions.jts.geom.impl.CoordinateArraySequence;
 import org.apache.spark.sql.*;
+import org.apache.spark.sql.expressions.MutableAggregationBuffer;
+import org.apache.spark.sql.expressions.UserDefinedAggregateFunction;
+import org.apache.spark.sql.types.DataType;
+import org.apache.spark.sql.types.StructType;
 import org.geotools.geojson.geom.GeometryJSON;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import scala.Tuple2;
 import scala.Tuple3;
+import scala.Tuple4;
 
 import java.io.FileReader;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 
@@ -23,6 +28,10 @@ import java.util.stream.Collectors;
 
 public class Task6 implements Serializable {
 
+    /**
+     * Read geojson file to JSONObject
+     * @return
+     */
     private static JSONObject readData(){
         JSONParser parser = new JSONParser();
         JSONObject data = null;
@@ -36,7 +45,11 @@ public class Task6 implements Serializable {
         return data;
     }
 
-
+    /**
+     * Create list of wrapped JSONObjec containing neighbourhood data using the Area class
+     * @param areas
+     * @return
+     */
     private static List<Area> createAreas(JSONArray areas){
         return (List<Area>) areas.stream()
                 .map((area) -> {
@@ -58,9 +71,10 @@ public class Task6 implements Serializable {
                 double lon = listing.getAs("longitude");
                 String city = listing.getAs("city");
                 String neighbourhood = findNeighbourhood( lon, lat, areas );
-                return new Tuple3<>( id, neighbourhood, city );
-            }, Encoders.tuple(Encoders.INT() ,Encoders.STRING(), Encoders.STRING()) )
-                .toDF("my_id", "my_neighbourhood", "my_city")
+                String amenities = listing.getAs("amenities");
+                return new Tuple4<>( id, neighbourhood, city, amenities );
+            }, Encoders.tuple(Encoders.INT() ,Encoders.STRING(), Encoders.STRING(), Encoders.STRING()) )
+                .toDF("my_id", "my_neighbourhood", "my_city", "amenities")
                 .orderBy(functions.col("my_id").asc());
     }
 
@@ -72,12 +86,37 @@ public class Task6 implements Serializable {
                         .col("my_id")
                         .equalTo(neighbourhoodTestSet.col("id"))
                         .as("id"));
+
         long count = joinedSet.count();
         long filteredCount = joinedSet
                 .filter( functions.col("neighbourhood").equalTo(functions.col("my_neighbourhood")) )
                 .count();
 
         System.out.println(String.format("All: %d, filtered: %d, percent: %d", count, filteredCount, filteredCount/count));
+    }
+
+    public static void distinctAmenitiesPerNeighbourhood(Dataset<Row> listings){
+        Dataset<Row> mappedListings = mapNeigbourhoodsToListings(listings);
+
+        mappedListings
+                .map( row -> {
+                    String amenities = ((String)row.getAs("amenities"))
+                            .replace("{", "")
+                            .replace("}", "")
+                            .replace("\"", "");
+                    String neighbourhood = row.getAs("my_neighbourhood");
+                    return new Tuple2<>( neighbourhood, amenities );
+                }, Encoders.tuple(Encoders.STRING(), Encoders.STRING()) )
+                .toJavaRDD()
+                .mapToPair( stringStringTuple2 -> new Tuple2<>( stringStringTuple2._1, stringStringTuple2._2 ) )
+
+                .reduceByKey( (s1, s2) -> {
+                    HashSet set = new HashSet<String>( Arrays.asList( s1.split(",") ) );
+                    set.addAll( Arrays.asList( s2.split(",") ) );
+                    return String.join(",", set);
+                })
+                .coalesce(1)
+                .saveAsTextFile("./output/yolo");
 
     }
 
