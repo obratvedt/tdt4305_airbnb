b1c9a937d7bdf1d02c9dd7a32d8b0cb588ea623d
Øyvind Bratvedt <oyvbr@stud.ntnu.no>
2017-03-28 14:13:23 +0200

    Cleaned descriptions

--- src/main/java/assignment2/ListingsTfIdf.java
+++ src/main/java/assignment2/ListingsTfIdf.java
@@ -2,11 +2,31 @@ package assignment2;
 
 
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.functions;
 
 public class ListingsTfIdf {
 
     public static void calculateListingsTfIdf(Dataset<Row> listingsDs, Dataset<Row> neighbourhoodsListingsDs, String listingId) {
-        //gogogo
+        Dataset<String> words = listingsDs
+                .select("description")
+                .filter(functions.col("id").equalTo(listingId))
+                .map(descRow -> {
+                    String description = (String) descRow.getAs("description");
+                    String descriptionCleaned = description.replaceAll("[^a-zA-Z ]", "").toLowerCase();
+                    return descriptionCleaned;
+                }, Encoders.STRING());
+
+        Dataset<String> listingDescriptions = listingsDs
+                .select("description")
+                .map(descRow -> {
+                    String description = (String) descRow.getAs("description");
+                    String descriptionCleaned = description.replaceAll("[^a-zA-Z ]", "").toLowerCase();
+                    return descriptionCleaned;
+                }, Encoders.STRING());
+
+        
+
     }
 }
