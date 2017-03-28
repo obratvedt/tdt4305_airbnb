abbec0d3a29d2c78323e8f7a1fcd17ae8f608e27
Øyvind Bratvedt <oyvbr@stud.ntnu.no>
2017-03-28 12:41:42 +0200

    Added project structure

---
+++ src/main/java/assignment2/ListingsTfIdf.java
@@ -0,0 +1,12 @@
+package assignment2;
+
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+
+public class ListingsTfIdf {
+
+    public static void calculateListingsTfIdf(Dataset<Row> listingsDs, Dataset<Row> neighbourhoodsListingsDs, String listingId) {
+        //gogogo
+    }
+}
---
+++ src/main/java/assignment2/NeighbourhoodsTfIdf.java
@@ -0,0 +1,11 @@
+package assignment2;
+
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+
+public class NeighbourhoodsTfIdf {
+    public static void calculateNeighbourhoodsTfIdf(Dataset<Row> listingsDs, Dataset<Row> neighbourhoodsListingsDs, String neighbourhood) {
+        //gogog
+    }
+}
